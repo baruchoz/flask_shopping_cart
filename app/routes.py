146368ae2aec30ca5dd56439cf22a7758806ba35
baruchoz <89609493+baruchoz@users.noreply.github.com>
2021-10-18 13:45:05 -0400

    registration form setup

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from app import app
-
+from flask import render_template
+from app.forms import RegistrationForm
 
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html', name='Baruch')
 
-@app.route('/test')
-def test():
-    return 'Test World'
+@app.route('/register')
+def register():
+    register_form = RegistrationForm()
+    return render_template('register.html', form=register_form)
